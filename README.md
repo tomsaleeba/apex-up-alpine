4e411ddae6ae0f329bc1a84731d87e02a69067e1
Tom Saleeba <tom.saleeba@gmail.com>
2018-04-17 11:14:30 +0930

    feat: add support for deleting stacks

--- README.md
+++ README.md
@@ -18,12 +18,20 @@ docker run \
   -e AWS_SECRET_ACCESS_KEY=<your secret here> \
   tomsaleeba/apex-up-alpine:0.1
 ```
+This will deploy to the `staging` stage of API Gateway. See below for when you want to deploy to `production`.
 
 ## Production deploys
 If you want to deploy to production, you should also supply an extra envrionmental variable:
 ```bash
-  -e IS_PROD=1 \
+  -e IS_PROD=1 \ # 0 = staging (default), 1 = production
 ```
+
+## Deleting a stack
+If you want to delete the stack (API Gateway, Lambda, etc) then supply this environment variable:
+```bash
+  -e IS_DELETE=1 \ # 0 = deploy (default), 1 = delete
+```
+Note: this will delete both `production` and `staging` stages AND everything else.
 
 ## Up profile value
 It doesn't matter if you define a `profile` in your `up.json`, this will still deploy appropriately. For reference, we define the AWS credentials with only a `default` entry.
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/sh
+[ -n "$IS_PROD" ] || IS_PROD=0
+[ -n "$IS_DELETE" ] || IS_DELETE=0
 the_dir=~/.aws
 mkdir -p $the_dir
 printf "[default]
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 " > $the_dir/credentials
-if [ $IS_PROD ]; then
-  extra_deploy_cmd="deploy production"
-  extra_url_cmd="production"
+if [ $IS_PROD -gt 0 ]; then
+  echo "Production mode is ON"
+  action="deploy"
+  env_fragment="production"
 fi
-echo "up version:"
-up version
-up $extra_deploy_cmd
-echo "URL of deployment:"
-up url $extra_url_cmd
+if [ $IS_DELETE -gt 0 ]; then
+  echo "Delete mode is ON"
+  if [ $IS_PROD -gt 0 ]; then
+    echo "Ignoring IS_PROD flag, 'stack delete' affects ALL stages"
+  fi
+  export CI=true # need this for the --format arg to work
+  action='stack delete --force --format="plain" --verbose'
+  env_fragment=""
+fi
+echo up version: $(up version)
+up $action $env_fragment
+if [ $IS_DELETE -le 0 ]; then
+  echo "URL of deployment:"
+  up url $env_fragment
+fi
